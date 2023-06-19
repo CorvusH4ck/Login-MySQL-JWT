c43802e0fdb0f52cf7bd3cb35acc2d9025897381
Corvus-DDoSKrom <corvusddoskrom@gmail.com>
2023-06-18 23:27:07 -0400

    update

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import indexRoutes from './routes/index.routes.js'
+import { PORT } from './config/config.js'
+
+const app = express()
+
+dotenv.config({ path: './env/.env' })
+
+app.use(cors())
+app.use(cookieParser())
+
+app.set('views', [ './public/views' ])
+app.set('view engine', 'ejs')
+
+app.use(express.static('./public'))
+app.use(express.static('./public/views'))
+app.use(express.static('./public/components'))
+app.use(express.static('./public/assets/css'))
+app.use(express.static('./public/assets/img'))
+app.use(express.static('./public/assets/js'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(function (req, res, next) {
+  if (!req.user) { res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate') }
+  next()
+})
+
+app.use(indexRoutes)
+
+app.listen(PORT)
+console.log('Server is listening on port', PORT)
